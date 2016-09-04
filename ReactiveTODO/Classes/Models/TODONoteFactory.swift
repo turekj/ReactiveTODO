db22656043ad35a15684db03f5cd87e1689f0102
Jakub Turek <j.turek@funandmobile.com>
2016-09-04 16:10:07 +0200

    Implemented TODONoteFactory

---
+++ ReactiveTODO/Classes/Models/TODONoteFactory.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+
+protocol TODONoteFactoryProtocol {
+    
+    func createTODONote() -> TODONote
+}
+
+
+class TODONoteFactory: TODONoteFactoryProtocol {
+
+    let dateResolver: DateResolverProtocol
+    let guidGenerator: GUIDGeneratorProtocol
+    
+    init(dateResolver: DateResolverProtocol,
+         guidGenerator: GUIDGeneratorProtocol) {
+        self.dateResolver = dateResolver
+        self.guidGenerator = guidGenerator
+    }
+    
+    func createTODONote() -> TODONote {
+        let note = TODONote()
+        note.date = self.dateResolver.now()
+        note.guid = self.guidGenerator.generateGUID()
+        
+        return note
+    }
+}
