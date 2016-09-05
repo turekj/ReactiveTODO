0d4ba640580b0f5a3d8daeb2259e041d3c149740
Jakub Turek <jkbturek@gmail.com>
2016-09-05 19:35:02 +0200

    Implemented TODOListAssembly stub

---
+++ ReactiveTODO/Classes/ViewModels/Factories/TODONoteListViewModelFactory.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+
+class TODONoteListViewModelFactory: TODONoteListViewModelFactoryProtocol {
+
+    func createViewModel() -> TODONoteListViewModel {
+        fatalError("Implement this")
+    }
+}
