141d5724c092878afec974092fd284a1da8771a7
Jakub Turek <jkbturek@gmail.com>
2016-09-05 17:01:36 +0200

    Dropped RootNavigationController class

--- ReactiveTODO/Classes/ViewControllers/RootNavigationControllerProtocol.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-
-protocol RootNavigationControllerProtocol {
-
-    func navigateToViewController<ViewController: UIViewController>(
-            controllerType: ViewController.Type, animated: Bool)
-    func navigateBackToRootViewController(animated: Bool)
-}
