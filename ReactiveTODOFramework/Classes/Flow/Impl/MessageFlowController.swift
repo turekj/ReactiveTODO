998c43d252cbc65e7636f99c6c53a823c3edb4b6
Jakub Turek <jkbturek@gmail.com>
2016-09-14 22:10:10 +0200

    Hidden add todo button in extension

--- ReactiveTODOFramework/Classes/Flow/Impl/MessageFlowController.swift
+++ ReactiveTODOFramework/Classes/Flow/Impl/MessageFlowController.swift
@@ -11,9 +11,16 @@ class MessageFlowController: MessageFlowControllerProtocol {
     }
     
     func startFlow(messageController: MessagesViewControllerProtocol) {
+        guard let list = messageController.noteListViewController
+                         as? TODONoteListViewController else {
+            return
+        }
+        
         messageController.noteListViewController?.onSelectTODO = { guid in
             let message = self.messageFactory.createMessage(guid)
             messageController.activeConversation?.insertMessage(message, completionHandler: nil)
         }
+        
+        list.notesView.addButton.hidden = true
     }
 }
