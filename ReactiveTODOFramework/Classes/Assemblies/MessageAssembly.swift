fd7f89b2c0a5edf8de4d88353679849411c83f69
Jakub Turek <jkbturek@gmail.com>
2016-09-14 20:15:46 +0200

    Created MessageAssembly container

---
+++ ReactiveTODOFramework/Classes/Assemblies/MessageAssembly.swift
@@ -0,0 +1,24 @@
+import Foundation
+import Swinject
+import UIKit
+
+
+public class MessageAssembly: AssemblyType {
+    
+    public init() { }
+    
+    public func assemble(container: Container) {
+        if #available(iOS 10.0, *) {
+            container.register(MessageFactoryProtocol.self) { r in
+                let bundle = NSBundle(forClass: MessageAssembly.self)
+                let todoNoteDAO = r.resolve(TODONoteDataAccessObjectProtocol.self)!
+                let dateFormatter = r.resolve(DateFormatterProtocol.self)!
+                let priorityFormatter = r.resolve(PriorityImageNameFormatterProtocol.self)!
+            
+                return MessageFactory(bundle: bundle, todoNoteDAO: todoNoteDAO,
+                                      dateFormatter: dateFormatter,
+                                      priorityFormatter: priorityFormatter)
+            }
+        }
+    }
+}
